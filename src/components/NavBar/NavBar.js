67d9131995f66e2e10bb4d700f4912409b2860af
samilieberman <samilieberman1@gmail.com>
2021-04-21 20:39:15 -0400

    Fixed navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,30 +1,17 @@
 import React, { Component } from "react";
 import { Nav, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Link } from "react-scroll";
 import { BrowserRouter, Link as DownloadLink } from "react-router-dom";
 import resume from '../../assets/Resume.pdf';
 class NavBar extends Component {
   render() {
     return (
         <Navbar expand="lg">
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="m-auto">
-              <Link to="/" activeClass="active" className="nav-link">
+            <BrowserRouter>
+              <DownloadLink to="/" activeClass="active" className="nav-link">
                 Home
-              </Link>
-              <Link
-                to="contact"
-                activeClass="active"
-                className="nav-link"
-                duration={1000}
-                spy={true}
-                smooth={true}
-              >
-                Contact
-              </Link>
-              <BrowserRouter>
+              </DownloadLink>
                 <DownloadLink
                   to={resume}
                   target="_blank"
@@ -35,7 +22,6 @@ class NavBar extends Component {
                 </DownloadLink>
               </BrowserRouter>
             </Nav>
-          </Navbar.Collapse>
         </Navbar>
     );
   }
