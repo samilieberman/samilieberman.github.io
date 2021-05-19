b46d5602aa3ad3a61b0b5f996f267db24cd1cdc2
samilieberman <samilieberman1@gmail.com>
2021-05-19 19:25:49 -0400

    added wip banner

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import NavBar from "../NavBar/NavBar";
 import Home from "../Home/Home";
@@ -11,15 +11,43 @@ import { ButtonWrapper, Wrapper, Sticky } from "./styles";
 import data from "../../data.json";
 
 function App() {
+  const [background, setBackground] = useState("red");
+
   useEffect(() => {
     window.scrollTo(0, 0);
   });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+      setBackground(randomColor);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  function handleHover(e) {
+    var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    setBackground(randomColor);
+  }
+
   return (
     <Wrapper>
+      <div
+        onMouseOver={handleHover}
+        style={{
+          textAlign: "center",
+          backgroundColor: `${background}`,
+          color: "white",
+          padding: "2rem",
+        }}
+      >
+        <strong> <span role="img" aria-label="wip"> 🚧🚧🚧 This is a WIP 🚧🚧🚧 </span> </strong> <br />
+        Hover over me!
+      </div>
       <Sticky>
         <NavBar />
         <Element name="home" className="element">
-        {data.map((item) => (  
+          {data.map((item) => (
           <Home name={item.name} description={item.description} />
         ))}
         </Element>
