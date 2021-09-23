46bd4dba4eb09217f98337e1613c0c56a43fa005
samilieberman <samilieberman1@gmail.com>
2021-09-23 11:56:42 -0400

    Updates

---
+++ precache-manifest.2bb2a721cd5ff1f598769b7f57c5d49e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4aeae689b417dc2f7d2e14647e709da",
+    "url": "/SL/index.html"
+  },
+  {
+    "revision": "a2e74883b70bdec8ff2b",
+    "url": "/SL/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "403476f337fc930efed6",
+    "url": "/SL/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a2e74883b70bdec8ff2b",
+    "url": "/SL/static/js/2.f3ffc318.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/SL/static/js/2.f3ffc318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "403476f337fc930efed6",
+    "url": "/SL/static/js/main.8c5e0804.chunk.js"
+  },
+  {
+    "revision": "ae3579762756f25b064d",
+    "url": "/SL/static/js/runtime-main.5c3c00c8.js"
+  },
+  {
+    "revision": "3daed682e30e6c3a1d163cb6c384fd13",
+    "url": "/SL/static/media/amex-logo.3daed682.svg"
+  },
+  {
+    "revision": "424c43e3b21b680b18c48ed11d8640d6",
+    "url": "/SL/static/media/gator.424c43e3.gif"
+  },
+  {
+    "revision": "bc316732176d4aeb0bac4765e53d8352",
+    "url": "/SL/static/media/projects2.bc316732.png"
+  },
+  {
+    "revision": "9482f74430ee57228349230013f943f2",
+    "url": "/SL/static/media/retro2.9482f744.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.587d5e7ed82d0c4f211b1eb5ec487b4c.js"
+  "/SL/precache-manifest.2bb2a721cd5ff1f598769b7f57c5d49e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SL/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
